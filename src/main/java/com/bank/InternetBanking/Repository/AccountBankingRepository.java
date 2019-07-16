89e44af77e388e502691f1c03037e27abdb90fcb
rksiet2 <rksiet2@gmail.com>
2019-07-16 10:58:16 +0530

    changing unused import

--- src/main/java/com/bank/InternetBanking/Repository/AccountBankingRepository.java
+++ src/main/java/com/bank/InternetBanking/Repository/AccountBankingRepository.java
@@ -1,7 +1,6 @@
 package com.bank.InternetBanking.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- src/main/java/com/bank/InternetBanking/Service/InternetBankingService.java
+++ src/main/java/com/bank/InternetBanking/Service/InternetBankingService.java
@@ -1,6 +1,5 @@
 package com.bank.InternetBanking.Service;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
